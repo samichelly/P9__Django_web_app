9453bd8a9effa4023185e7f4567d829122d4f055
Sami Chelly <samichelly15@gmail.com>
2023-09-20 16:01:41 +0200

    Corrections front-end : pages subscription, signin, and create_ticket

--- LITReview/app_books/forms.py
+++ LITReview/app_books/forms.py
@@ -15,12 +15,27 @@ class SignUpForm(UserCreationForm):
 
 
 class SignInForm(AuthenticationForm):
+    username = forms.CharField(
+        widget=forms.TextInput(attrs={"placeholder": "Nom d'utilisateur"})
+    )
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={"placeholder": "Mot de passe"})
+    )
+
     class Meta:
         model = User
         fields = ("username", "password")
 
 
 class TicketForm(forms.ModelForm):
+    title = forms.CharField(
+        widget=forms.TextInput(attrs={"placeholder": "Ajouter un titre"})
+    )
+    description = forms.CharField(
+        widget=forms.Textarea(attrs={"placeholder": "Ajouter une description"})
+    )
+    image = forms.ImageField(label="Sélectionner une image")
+
     class Meta:
         model = Ticket
         fields = ("title", "description", "image")
